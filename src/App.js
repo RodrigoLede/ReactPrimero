c875b3447a0a3e1f3f0f51743d2baab831f66f48
Rodri Lede <lededami.rodrigo@gmail.com>
2023-02-01 19:36:00 -0300

    Clase 3

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "¡¡¡¡¡Bienvenidos a React!!!!!";
+
+  const producto= {
+    precio: 500,
+    nombre: "Remera",
+    descripcion: "La remera",
+  };
+
+  function hacerAlgo() {
+    alert("hola");
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {title}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div>
+              <h3> {producto.nombre} </h3>
+              <h3>$ {producto.precio} </h3>
+            </div>
+          </a>
+        </header>
+      </div>
+      <button onClick={hacerAlgo}>Click Me</button>
     </div>
   );
 }
