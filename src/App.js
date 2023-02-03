3d48ffd5fcf11863c1a3ecb7e78b230d34d3d9f6
Rodri Lede <lededami.rodrigo@gmail.com>
2023-02-03 01:19:02 -0300

    Clase 6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css';
-import Card from "./componentes/Card"
 import Button from './componentes/Button/Button';
 import NavBar from './componentes/NavBar/NavBar';
 import ItemCount from './componentes/ItemCount/ItemCount';
@@ -11,23 +10,12 @@ function App() {
   function hacerAlgo() {
     alert("hola");
   }
-  const miEstilo={
-    backgroundColor: "green",
-    margin: "30px",
-    padding: "50px",
-    textAlign: "center",
-  };
+  
   return (
     <div>
       <NavBar />
 
       <ItemListContainer />
-
-      <div style={miEstilo} className="catalogo">
-        <Card img= "/imgs/Captura.jpg" title= "Stumpjumper" price= {500} detail= "Expert pro"/>
-        <Card img= "/imgs/Captura1.jpg" title= "Enduro" price= {800} detail= "Expert pro"/>
-        <Card img= "/imgs/Captura2.jpg" title= "Turbo Levo" price= {600} detail= "Expert pro"/>
-      </div>
 
       <ItemCount stock={8} />
 
--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import {useState, useEffect} from "react";
+import Card from "../Card";
+
+
 
 export default function ItemListContainer() {
+
+  const miEstilo={
+    backgroundColor: "green",
+    margin: "30px",
+    padding: "50px",
+    textAlign: "center",
+  };
+
   return (
-    <div>ItemListContainer</div>
+    <div style={miEstilo} className="catalogo">
+        <Card img= "/imgs/Captura.jpg" title= "Stumpjumper" price= {500} detail= "Expert pro"/>
+        <Card img= "/imgs/Captura1.jpg" title= "Enduro" price= {800} detail= "Expert pro"/>
+        <Card img= "/imgs/Captura2.jpg" title= "Turbo Levo" price= {600} detail= "Expert pro"/>
+      </div>
   )
 }
